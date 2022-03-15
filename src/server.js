152baf78dff47d1307adaf76da91f047aa8641d4
macindex <machollanda@gmail.com>
2022-03-15 10:54:25 -0300

    http methods

--- src/server.js
+++ src/server.js
@@ -1,9 +1,34 @@
 import { createServer } from 'http'
+import { once } from 'events'
+import { randomUUID } from 'crypto'
+const Database = new Map()
 
-
+function respondJSON(data, response){
+    return response.end(JSON.stringify(data))
+}
 
 async function handler(request, response){
+    const { method } = request
+    
+    if(method === 'GET'){
+
+        return respondJSON([...Database.values()], response)
+    }
+    if(method === 'POST'){
+        const body = JSON.parse(await once(request, 'data'))
+        console.log('recebido', body)
+        const id = randomUUID()
+        Database.set(id, body)
+        
+        return respondJSON({ ok: 1 }, response)
+    }
+    if(method === 'DELETE'){
+
+        return respondJSON({ ok: 1 }, response)
+    }
+
     response.end('hello word')
 }
+
 
 export default createServer(handler)
