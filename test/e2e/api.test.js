25f4a6e9ef6eae30e5e9cb9d23b263019d700ebb
macindex <machollanda@gmail.com>
2022-03-21 11:59:04 -0300

    create test and env

---
+++ test/e2e/api.test.js
@@ -0,0 +1,8 @@
+import { jest, expect, test, describe } from '@jest/globals'
+
+describe('API E2E Test Suite', () => {
+    test.todo('GET / - Should return an array')
+    test.todo('POST / - Should save an item and return ok')
+    test.todo('DELETE / - Should delete all itens and return ok')
+})
+
