1f9ac34db3eeacf91fafa8d9b8d8da8d9abcf8aa
macindex <machollanda@gmail.com>
2022-03-22 20:42:52 -0300

    tests in get route

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -1,7 +1,17 @@
 import { jest, expect, test, describe } from '@jest/globals'
 
+import superTest from 'supertest'
+import Server from '../../src/server.js' 
 describe('API E2E Test Suite', () => {
-    test.todo('GET / - Should return an array')
+    test('GET / - Should return an array', async () => {
+        const response = await superTest(Server)
+        .get('/')
+
+        const data = JSON.parse(response.text)
+        expect(data).toBeInstanceOf(Array)
+        //console.log('text', response.text)
+    })
+    
     test.todo('POST / - Should save an item and return ok')
     test.todo('DELETE / - Should delete all itens and return ok')
 })
